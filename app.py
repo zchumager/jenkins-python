aff3e416cb643bb73a6f73e958c6930611258856
Pedro Arnoldo Machado Duran <pmachado@tiempodevelopment.com>
2021-07-07 22:27:43 -0600

    get jsonplaceholder

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import requests
+from pprint import pprint
 
 def main():
     print("Executing Python Module")
+
+    result = requests.get('https://jsonplaceholder.typicode.com/posts')
+    pprint(result.json())
 
 if __name__ == "__main__":
     main()

d6c167701cb02a79387a8c1e3a271728f90cb40b
Pedro Arnoldo Machado Duran <pmachado@tiempodevelopment.com>
2021-07-07 21:42:35 -0600

    adding pipenv

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 def main():
-    print("Executing Python script")
+    print("Executing Python Module")
 
 if __name__ == "__main__":
     main()
--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
-echo "starting script"
+echo "Executing Shellscript"
+
+python3 -m pip install --upgrade pip setuptools wheel
+
+python3 -m pip install --user pipenv
 
 python3 app.py
